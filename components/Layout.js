f63753b27e6192ea6402b3f4a26fb9639f79f7c0
Mohammad <mohammadmahone@gmail.com>
2022-05-28 12:54:45 +0430

    - Initial commit.

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+import Header from "./Header";
+
+const Layout = ({ title, children, keywords, description }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main className="container mx-auto my-7">{children}</main>
+    </div>
+  );
+};
+
+Layout.defaultProps = {
+  title: "Welcome to DevSpace",
+  keywords: "development, codingm programming",
+  description: " The best info in the development",
+};
+
+export default Layout;
---
+++ pages/blog/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+const BlogPage = () => {
+  return (
+    <Layout title="Blog">
+      <h1>Blog</h1>
+    </Layout>
+  );
+};
+
+export default BlogPage;
