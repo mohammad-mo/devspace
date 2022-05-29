a3335e75d63db67cbe2302adf6cab854ed62c8be
Mohammad <mohammadmahone@gmail.com>
2022-05-29 11:41:38 +0430

    - Get posts from markdown files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
-import Layout from "../components/Layout";
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
-export default function HomePage() {
+import Layout from '../components/Layout'
+
+export default function HomePage({ posts }) {
   return (
     <Layout>
       <h1>Hello word</h1>
     </Layout>
-  );
+  )
+}
+
+export async function getStaticProps() {
+  // Reading the posts from posts directory
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+
+    // To show the content of markdown files
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8',
+    )
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
